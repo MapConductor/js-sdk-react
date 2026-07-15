cb4df7fc61d51ef2f0e79d0f3f39c2572cbfe0b3
Masashi Katsumata <wf9a5m75@gmail.com>
2026-07-16 03:47:27 +0900

    ignore .cxx cache folder

--- android/.cxx/Debug/483j6fy6/prefab/x86/prefab/lib/i686-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/Users/masashi/.gradle/caches/9.2.1/transforms/2604e7cc6ba524fe17581d67dabddcd5/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/masashi/.gradle/caches/9.2.1/transforms/2604e7cc6ba524fe17581d67dabddcd5/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
